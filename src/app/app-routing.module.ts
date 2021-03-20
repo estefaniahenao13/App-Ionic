03eeff765c9ae2df719842f962ad18b46486fd0e
Estefania Henao <estefaniahenao589@gmail.com>
2021-03-20 10:11:58 -0500

    Creacion componente header-modulos y botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,17 @@ const routes: Routes = [
     path: 'inicio',
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
+  
+  {
+    path: 'alert',
+    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
+  },
+
+  {
+    path: 'action-sheet',
+    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
+  },
+  
   {
     path: '',
     redirectTo: 'inicio',
