a59040673e8e12800b6e1f92eb7c7da8128d10a7
Estefania Henao <estefaniahenao589@gmail.com>
2021-03-27 09:37:11 -0500

    creacion botones y categorias

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent{
+
+  @Input() titulo: string = "";
 
   constructor() { }
 
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'alert-outline',
+      name: 'Alert',
+      redirectTo: '/alert'
+
+    },
+    {
+      icon: 'football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+
+    }
+  ];
 
   constructor() { }
 
